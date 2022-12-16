92ad1affcf7f2af15bb53e0b8797a4d71fe599f8
Sleyther <sleyther.calsin@cloudcomputing.com.pe>
2022-12-16 15:55:24 -0500

    Instancias parte 2

--- src/CrearCuenta.java
+++ src/CrearCuenta.java
@@ -2,6 +2,11 @@ public class CrearCuenta {
  public static void main(String[] args) {
   Cuenta primeraCuenta = new Cuenta();
   primeraCuenta.saldo = 1000;
+//  primeraCuenta.pais = "Peru"; No compila
   System.out.println(primeraCuenta.saldo);
+  
+  Cuenta segundaCuenta = new Cuenta();
+  segundaCuenta.saldo = 500;
+  System.out.println(segundaCuenta.saldo);
  }
 }

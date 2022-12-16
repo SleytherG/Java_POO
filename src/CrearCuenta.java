6dc27be073ac1f91bdae6a2d5bf738707262a4ca
Sleyther <sleyther.calsin@cloudcomputing.com.pe>
2022-12-16 16:03:35 -0500

    Referencias parte 1

--- src/CrearCuenta.java
+++ src/CrearCuenta.java
@@ -1,5 +1,6 @@
 public class CrearCuenta {
  public static void main(String[] args) {
+  // Variable         =  Valor
   Cuenta primeraCuenta = new Cuenta();
   primeraCuenta.saldo = 1000;
 //  primeraCuenta.pais = "Peru"; No compila
@@ -8,5 +9,7 @@ public class CrearCuenta {
   Cuenta segundaCuenta = new Cuenta();
   segundaCuenta.saldo = 500;
   System.out.println(segundaCuenta.saldo);
+  System.out.println(primeraCuenta.agencia);
+  
  }
 }
